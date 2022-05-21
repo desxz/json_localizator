b10a33553bcc274f1b4502d3bc1dbe4ff6fc49eb
desxz <muratgun545@gmail.com>
2022-05-22 01:21:30 +0300

    edit test

--- tests/json_localizator_test.dart
+++ tests/json_localizator_test.dart
@@ -8,7 +8,7 @@ import 'dart:io' as io;
 import '../bin/json_localizator.dart';
 
 Future<void> main() async {
-  test('should json file localize', () async {
+  test('should single json file localize', () async {
     final localizator = JsonLocalizator(GoogleTranslator());
     final sourceFile = io.File('tests/dummy-data/dummy-data.json');
     final expectedPath = 'tests/dummy-data/fr-FR.json';
@@ -26,5 +26,35 @@ Future<void> main() async {
             .readAsString()
             .then((value) => value.isNotEmpty));
     await io.File(expectedPath).delete();
+  });
+
+  test('should multi json file localize', () async {
+    final localizator = JsonLocalizator(GoogleTranslator());
+    final sourceFile = io.File('tests/dummy-data/dummy-data.json');
+    final expectedPath = 'tests/dummy-data/fr-FR.json';
+    final expectedPath2 = 'tests/dummy-data/tr-TR.json';
+    final jsonFileSource =
+        sourceFile.readAsString().then((data) => jsonDecode(data));
+    final jsonFileData = await jsonFileSource as Map<String, dynamic>;
+
+    await localizator.generateLocalizedJson(
+        jsonFileData, 'en', 'fr', 'tests/dummy-data');
+    await localizator.generateLocalizedJson(
+        jsonFileData, 'en', 'tr', 'tests/dummy-data');
+
+    expect(true, await io.File(expectedPath).exists());
+    expect(true, await io.File(expectedPath2).exists());
+    expect(
+        true,
+        await io.File(expectedPath)
+            .readAsString()
+            .then((value) => value.isNotEmpty));
+    expect(
+        true,
+        await io.File(expectedPath2)
+            .readAsString()
+            .then((value) => value.isNotEmpty));
+    await io.File(expectedPath).delete();
+    await io.File(expectedPath2).delete();
   });
 }
